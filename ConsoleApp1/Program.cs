f844f2eca2c9638f01c45f658977b77cdf300666
KristinaAnichkina <156066393+KristinaAnichkina@users.noreply.github.com>
2024-07-30 20:13:53 +0800

    init

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+   static void Main(string[] args)
+   {
+       int M = 1;
+       int N = 5;
+       PrintNumbersInRange(M, N);
+   }
+   static void PrintNumbersInRange(int start, int end)
+   {
+       if (start <= end)
+       {
+           Console.Write(start);
+           if (start < end)
+               Console.Write(", ");
+           PrintNumbersInRange(start + 1, end);
+       }
+   }
+}
